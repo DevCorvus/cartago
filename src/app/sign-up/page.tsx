c4b9b7260f4e2eff72d7e363c8b3f2e023a3bb65
DevCorvus <devcorvus.contact@gmail.com>
2023-10-04 16:47:51 -0400

    feat: make sign-up form work and fix typos and formatting

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,22 +1,64 @@
-'use client'
+'use client';
 
+import { signIn } from 'next-auth/react';
 import Link from 'next/link';
-import { ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function SignUp() {
-  const [displayConfirmPassword, setDisplayConfirmPassword] = useState(false)
+  const router = useRouter();
+  const [displayConfirmPassword, setDisplayConfirmPassword] = useState(false);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (e.target instanceof HTMLFormElement) {
+      const formData = new FormData(e.target);
+
+      const fullname = formData.get('fullname');
+      const email = formData.get('email');
+      const password = formData.get('password');
+
+      try {
+        const signUpRes = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ fullname, email, password }),
+        });
+
+        if (!signUpRes.ok) throw new Error('User not created');
+
+        const signInRes = await signIn('credentials', {
+          email,
+          password,
+          redirect: false,
+        });
+
+        if (signInRes?.ok && !signInRes.error) {
+          return router.push('/');
+        } else {
+          console.log(signInRes?.error);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
+    const value = e.target.value;
     if (!displayConfirmPassword && value.length >= 6) {
-      setDisplayConfirmPassword(true)
+      setDisplayConfirmPassword(true);
     }
-  }
+  };
 
   return (
     <div className="bg-lime-50 w-full h-full flex items-center justify-center text-green-800">
       <div className="flex flex-col gap-12">
         <form
+          onSubmit={handleSubmit}
           className="flex items-center justify-center flex-col gap-10"
         >
           <header className="w-full ">
@@ -48,9 +90,17 @@ export default function SignUp() {
               />
             </div>
             <div className="flex  gap-2 items-center">
-              <label htmlFor="location" className="opacity-50">Location</label>
-              <select name="location" id="location" className="p-1 opacity-75 border border-lime-700 rounded-lg outline-none ">
-                <option defaultValue={""} disabled>Select country</option>
+              <label htmlFor="location" className="opacity-50">
+                Location
+              </label>
+              <select
+                name="location"
+                id="location"
+                className="p-1 opacity-75 border border-lime-700 rounded-lg outline-none"
+              >
+                <option defaultValue={''} disabled>
+                  Select country
+                </option>
                 <option value="peru">Peru</option>
                 <option value="venezuela">Venezuela</option>
                 <option value="colombia">Colombia</option>
@@ -70,7 +120,11 @@ export default function SignUp() {
                 onChange={handlePasswordChange}
               />
             </div>
-            <div className={`${displayConfirmPassword ? 'flex' : 'hidden'} justify-center flex-col gap-2 `}>
+            <div
+              className={`${
+                displayConfirmPassword ? 'flex' : 'hidden'
+              } justify-center flex-col gap-2`}
+            >
               <label htmlFor="confirmPassword" className="opacity-50">
                 Confirm password
               </label>
@@ -87,12 +141,18 @@ export default function SignUp() {
             type="submit"
             className="bg-green-800 p-3 w-full rounded-3xl text-slate-50 shadow-lg"
           >
-            Sing Up
+            Sign Up
           </button>
         </form>
         <div className="flex items-center justify-center flex-col text-sm opacity-75">
           <p>
-            Do you already have an account? <Link href="sign-in" className="hover:text-lime-700 transition font-semibold">Sing In</Link>
+            Do you already have an account?{' '}
+            <Link
+              href="sign-in"
+              className="hover:text-lime-700 transition font-semibold"
+            >
+              Sign In
+            </Link>
           </p>
         </div>
       </div>
