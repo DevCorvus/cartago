de921e40d89f3ed5f19756fa9f1b306ff1ebaa48
Lachicagladiadora <200379@unsaac.edu.pe>
2023-10-12 09:45:08 -0500

    wip(product-item-details): add image styles

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -9,8 +9,14 @@ export default function ProductItem() {
       createdAt="25/03/2023"
       description="loresdfhnkdjlsgvkdfmgkl"
       stock={4}
-      tags={[]}
-      images={[]}
+      tags={['clothes', 'shirt']}
+      images={[
+        'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/8499861/pexels-photo-8499861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/6920411/pexels-photo-6920411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      ]}
     />
   );
 }
--- src/components/ui/ProductItemDetails.tsx
+++ src/components/ui/ProductItemDetails.tsx
@@ -27,24 +27,31 @@ export default function ProductItemDetails({
   const [selectedImage, setSelectedImage] = useState(images[0]);
   return (
     <div>
-      <div className="w-full h-96 relative">
-        <Image
-          src={selectedImage}
-          alt={`${name} selected image`}
-          className="w-full h-full border border-red-600"
-        />
-        <div className="">
+      <div className="w-full h-96 relative flex flex-col border border-red-600">
+        <div className="relative h-4/5">
+          <Image
+            src={selectedImage}
+            fill={true}
+            objectFit="contain"
+            alt={`${name} selected image`}
+          />
+        </div>
+        <div className="flex-1 flex border border-green-600 relative">
           {images.map((url, i) => (
-            <Image
-              src={url}
-              alt={`${name} image #${i + 1}`}
-              onClick={() => {
-                setSelectedImage(url);
-              }}
-            />
+            <div className="relative w-20 h-full" key={i}>
+              <Image
+                src={url}
+                alt={`${name} image #${i + 1}`}
+                fill={true}
+                objectFit="contain"
+                onClick={() => {
+                  setSelectedImage(url);
+                }}
+              />
+            </div>
           ))}
         </div>
-        <button className="absolute top-2 right-2 z-10">
+        <button className="absolute top-2 right-2 z-10 text-base">
           <HiStar />
         </button>
       </div>
@@ -54,7 +61,7 @@ export default function ProductItemDetails({
         </header>
         <p>$ {price}</p>
       </div>
-      <section className="text-sm flex flex-col p-2">
+      <section className="text-sm flex flex-col p-2 h-32 justify-between">
         <p className="py-1">{description}</p>
         <div className="flex justify-between">
           <p>{stock} in stock</p>
@@ -62,8 +69,8 @@ export default function ProductItemDetails({
         </div>
       </section>
       <div>
-        {tags.map((tag) => (
-          <span className="border border-red-600 w-9 h-6 rounded-full">
+        {tags.map((tag, i) => (
+          <span key={i} className="border border-red-600 w-9 h-6 rounded-full">
             {tag}
           </span>
         ))}
