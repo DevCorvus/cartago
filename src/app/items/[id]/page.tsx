da2d1bc28f77cb27ea9ba0994ec5b9d483c00a08
Lachicagladiadora <200379@unsaac.edu.pe>
2023-10-26 10:27:13 -0500

    refactor(product-item): passing props as unified object

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -3,29 +3,32 @@ import ProductItemDetails from '@/components/ui/ProductItemDetails';
 export default function ProductItem() {
   return (
     <ProductItemDetails
-      id="3243"
-      name="shoes"
-      price="100.00"
-      createdAt="25/03/2023"
-      description="Sonic would like these ones"
-      stock={4}
-      tags={[
-        'Books',
-        'Home Appliances',
-        'Music',
-        'Tech',
-        'Health',
-        'Pets',
-        'Art',
-        'Jewelry',
-      ]}
-      images={[
-        'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        'https://images.pexels.com/photos/8499861/pexels-photo-8499861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        'https://images.pexels.com/photos/6920411/pexels-photo-6920411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        'https://images.pexels.com/photos/318236/pexels-photo-318236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      ]}
+      product={{
+        id: '3243',
+        title: 'shoes',
+        price: 100.0,
+        description: 'Sonic would like these ones',
+        stock: 8,
+        tags: [
+          'Books',
+          'Home Appliances',
+          'Music',
+          'Tech',
+          'Health',
+          'Pets',
+          'Art',
+          'Jewelry',
+        ],
+        images: [
+          'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'https://images.pexels.com/photos/8499861/pexels-photo-8499861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'https://images.pexels.com/photos/18542177/pexels-photo-18542177/free-photo-of-moda-zapatos-monitor-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'https://images.pexels.com/photos/6920411/pexels-photo-6920411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          'https://images.pexels.com/photos/318236/pexels-photo-318236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ],
+        createdAt: '25/03/2023',
+        updatedAt: '26/03/2023',
+      }}
     />
   );
 }
