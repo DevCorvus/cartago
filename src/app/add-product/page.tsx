6ade10aad3d538964d0d7f4b53bf1b8144ddfb02
Lachicagladiadora <200379@unsaac.edu.pe>
2023-10-30 10:41:53 -0500

    wip: make add product form work

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,10 +1,51 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { FormEvent } from 'react';
+
 export default function AddProduct() {
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (e.target instanceof HTMLFormElement) {
+      const formData = new FormData(e.target);
+
+      const title = formData.get('title');
+      const description = formData.get('description');
+      const price = Number(formData.get('price'));
+      const stock = Number(formData.get('stock'));
+
+      try {
+        const res = await fetch('/api/products', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'aplication/json',
+          },
+          body: JSON.stringify({
+            title,
+            description,
+            price,
+            stock,
+            images: [],
+            categories: [],
+          }),
+        });
+        if (res.ok) {
+          return router.push('/items/abc');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="p-5 pt-12 bg-lime-50 w-full h-full flex flex-col gap-5 items-center justify-center text-green-800">
       <form
         action=""
+        onSubmit={handleSubmit}
         className="flex items-center justify-center flex-col gap-6"
       >
         <header className="w-full ">
@@ -19,18 +60,43 @@ export default function AddProduct() {
             />
           </div>
           <div className="flex flex-col gap-3">
-            <label htmlFor="">Name</label>
+            <label htmlFor="title">Title</label>
             <input
+              id="title"
+              name="title"
               type="text"
-              placeholder="write name product"
+              placeholder="write product title"
               className="rounded-lg p-4 outline-none text-sm shadow-md"
             />
           </div>
           <div className="flex flex-col gap-3">
-            <label htmlFor="">Price</label>
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              cols={30}
+              rows={5}
+              placeholder="write description"
+              className="rounded-lg p-4 outline-none text-sm shadow-md"
+            ></textarea>
+          </div>
+          <div className="flex flex-col gap-3">
+            <label htmlFor="price">Price</label>
             <input
-              type="text"
+              id="price"
+              name="price"
+              type="number"
               placeholder="write price product"
+              className="rounded-lg p-4 outline-none text-sm shadow-md"
+            />
+          </div>
+          <div className="flex flex-col gap-3">
+            <label htmlFor="stock">Stock</label>
+            <input
+              id="stock"
+              name="stock"
+              type="number"
+              placeholder="write stock of product"
               className="rounded-lg p-4 outline-none text-sm shadow-md"
             />
           </div>
@@ -41,17 +107,6 @@ export default function AddProduct() {
               placeholder="write categories product"
               className="rounded-lg p-4 outline-none text-sm shadow-md"
             />
-          </div>
-          <div className="flex flex-col gap-3">
-            <label htmlFor="">Description</label>
-            <textarea
-              name="description"
-              id=""
-              cols={30}
-              rows={10}
-              placeholder="write description"
-              className="rounded-lg p-4 outline-none text-sm shadow-md"
-            ></textarea>
           </div>
         </div>
         <div className="w-full">
