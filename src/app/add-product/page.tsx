9861ac3b597c0fba0cc9e7a9d69b26f4085bc923
Lachicagladiadora <200379@unsaac.edu.pe>
2023-11-02 09:51:05 -0500

    wip: add image previews with react-dropzone

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,11 +1,39 @@
 'use client';
 
 import { ProductDto } from '@/shared/dtos/product.dto';
+import Image from 'next/image';
+import { FormEvent, useCallback, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { FormEvent } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+interface FilePreview {
+  name: string;
+  url: string;
+}
 
 export default function AddProduct() {
   const router = useRouter();
+  const [files, setFiles] = useState<File[]>([]);
+  const [filePreviews, setFilePreviews] = useState<FilePreview[]>([]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        setFiles((prev) => [...prev, file]);
+        setFilePreviews((prev) => [
+          ...prev,
+          { name: file.name, url: URL.createObjectURL(file) },
+        ]);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -29,7 +57,6 @@ export default function AddProduct() {
             description,
             price,
             stock,
-            images: [],
             categories: [],
           }),
         });
@@ -44,7 +71,7 @@ export default function AddProduct() {
   };
 
   return (
-    <div className="p-5 pt-12 bg-lime-50 w-full h-full flex flex-col gap-5 items-center justify-center text-green-800">
+    <div className="p-5 pt-30 bg-lime-50 w-full h-full flex flex-col gap-5 items-center justify-center text-green-800">
       <form
         action=""
         onSubmit={handleSubmit}
@@ -54,13 +81,31 @@ export default function AddProduct() {
           <h1 className=" text-2xl font-bold">Add Product</h1>
         </header>
         <div className="flex flex-col gap-4 w-auto">
-          <div className="w-full flex flex-col gap-3">
-            <label htmlFor="">Image</label>
-            <input
-              type="image"
-              className="w-full rounded-lg p-4 outline-none text-sm shadow-md"
-            />
+          <div
+            {...getRootProps()}
+            className="border border-green-700 border-opacity-30 p-4 rounded-md"
+          >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>Drag and drop some files here or click to select files</p>
+            )}
           </div>
+          {files.length > 0 && (
+            <div className=" flex gap-1 border border-green-700 border-opacity-30 p-1 rounded-md">
+              {filePreviews.map((file, i) => (
+                <div key={i + 1} className="relative w-16 h-16">
+                  <Image
+                    src={file.url}
+                    alt={file.name}
+                    fill={true}
+                    object-fit="contain"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
           <div className="flex flex-col gap-3">
             <label htmlFor="title">Title</label>
             <input
