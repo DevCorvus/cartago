76db68a8f3d86f0bba2695d9e99d256fe164d570
DevCorvus <devcorvus.contact@gmail.com>
2023-11-15 12:10:00 -0400

    feat: category tag input autocomplete

--- src/components/ui/CategoryTagsInput.tsx
+++ src/components/ui/CategoryTagsInput.tsx
@@ -8,6 +8,7 @@ import {
   useState,
   KeyboardEvent,
   useEffect,
+  useMemo,
 } from 'react';
 import { HiXMark } from 'react-icons/hi2';
 
@@ -26,10 +27,31 @@ export default function CategoryTagsInput({
   const [autocompleteCategoryTags, setAutocompleteCategoryTags] = useState<
     CategoryTag[]
   >([]);
+  const [input, setInput] = useState<string>('');
 
   useEffect(() => {
     setCategoryIds(categoryTags.map((tag) => tag.id));
   }, [categoryTags, setCategoryIds]);
+
+  useEffect(() => {
+    if (input) {
+      const lowerInput = input.toLowerCase();
+
+      setAutocompleteCategoryTags(
+        categoryTags.filter((tag) => {
+          const tagAlreadyExist = selectedCategoryTags.some(
+            // It should be selected by id
+            (selectedTag) => selectedTag.title === tag.title,
+          );
+          const tagStartsWith = tag.title.toLowerCase().startsWith(lowerInput);
+
+          return !tagAlreadyExist && tagStartsWith;
+        }),
+      );
+    } else {
+      setAutocompleteCategoryTags([]);
+    }
+  }, [input, categoryTags, selectedCategoryTags]);
 
   const handleKey = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.target instanceof HTMLInputElement) {
@@ -38,30 +60,44 @@ export default function CategoryTagsInput({
       if (e.key === 'Enter') {
         e.preventDefault();
 
-        setSelectedCategoryTags((prev) => [
-          ...prev,
-          { id: prev.length + 1, title: value },
-        ]);
+        if (value) {
+          const lowerInput = input.toLowerCase();
+          setInput('');
 
-        e.target.value = '';
+          const selectedTagAlreadyExist = selectedCategoryTags.some(
+            (tag) => tag.title.toLowerCase() === lowerInput,
+          );
+
+          if (selectedTagAlreadyExist) return;
+
+          const categoryMatch = categoryTags.find(
+            (tag) => tag.title.toLowerCase() === lowerInput,
+          );
+
+          if (categoryMatch) {
+            setSelectedCategoryTags((prev) => [...prev, { ...categoryMatch }]);
+          } else {
+            // TODO: Create new category
+            setSelectedCategoryTags((prev) => [
+              ...prev,
+              { id: prev.length + 1, title: value },
+            ]);
+          }
+
+          setAutocompleteCategoryTags([]);
+        }
       } else if (e.key == 'Backspace' && !value) {
         setSelectedCategoryTags((prev) =>
-          categoryTags.filter((_, i) => i !== prev.length - 1),
+          selectedCategoryTags.filter((_, i) => i !== prev.length - 1),
         );
-      }
-
-      if (value) {
-        const lowerCaseValue = value.toLowerCase();
-
-        setAutocompleteCategoryTags(
-          categoryTags.filter((tag) =>
-            tag.title.toLowerCase().startsWith(lowerCaseValue),
-          ),
-        );
-      } else {
-        setAutocompleteCategoryTags([]);
       }
     }
+  };
+
+  const handleSelectAutocomplete = (tag: CategoryTag) => {
+    setSelectedCategoryTags((prev) => [...prev, { ...tag }]);
+    setAutocompleteCategoryTags([]);
+    setInput('');
   };
 
   const handleDelete = (id: number) => {
@@ -71,12 +107,17 @@ export default function CategoryTagsInput({
   return (
     <div className="flex flex-col gap-3 w-72 relative">
       <label htmlFor="categories">Categories</label>
-      <ul>
-        {autocompleteCategoryTags.map((tag) => (
-          <li key={tag.id}>{tag.title}</li>
-        ))}
-      </ul>
-
+      {autocompleteCategoryTags.length > 0 && (
+        <ul>
+          {autocompleteCategoryTags.map((tag) => (
+            <li key={tag.id}>
+              <button onClick={() => handleSelectAutocomplete(tag)}>
+                {tag.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
       <ul className="flex flex-wrap gap-1 rounded-lg p-3 text-sm bg-white shadow-md">
         {selectedCategoryTags.map((selectedTag) => (
           <li key={selectedTag.id}>
@@ -97,6 +138,8 @@ export default function CategoryTagsInput({
             className="outline-none p-1 bg-none w-28"
             placeholder="Add category..."
             onKeyDown={handleKey}
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
           />
         </li>
       </ul>
