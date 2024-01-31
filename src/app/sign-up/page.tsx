ac57c874094dead180c2a604d2cc0f1f4029c89c
DevCorvus <devcorvus.contact@gmail.com>
2024-01-31 10:28:06 -0400

    feat: protect routing to sign-in and sign-up pages

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,15 @@
 import SignUpForm from '@/components/ui/SignUpForm';
+import { getUserSession } from '@/server/auth/auth.utils';
 import { countryService } from '@/server/services';
+import { redirect } from 'next/navigation';
 
 export default async function SignUp() {
+  const user = await getUserSession();
+
+  if (user) {
+    redirect('/');
+  }
+
   const countries = await countryService.findAll();
 
   return (
---
+++ src/components/ui/SignInForm.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { LoginUserDto } from '@/shared/dtos/user.dto';
+import { loginUserSchema } from '@/shared/schemas/user.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signIn } from 'next-auth/react';
+import Link from 'next/link';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+export function SignInForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginUserDto>({ resolver: zodResolver(loginUserSchema) });
+
+  const onSubmit: SubmitHandler<LoginUserDto> = async (data) => {
+    await signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      redirect: true,
+      callbackUrl: '/',
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex items-center justify-center flex-col gap-10 max-w-sm"
+      >
+        <header className="w-full">
+          <h1 className="text-2xl font-bold text-green-800">Sign In</h1>
+        </header>
+        <div className="flex flex-col gap-6 w-full">
+          <div className="flex justify-center flex-col gap-2">
+            <label htmlFor="email" className="text-green-800 opacity-75">
+              Email
+            </label>
+            <input
+              {...register('email')}
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              className="rounded-lg p-4 outline-none shadow-md"
+            />
+            {errors.email && (
+              <p className="text-red-400">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="flex justify-center flex-col gap-2">
+            <label htmlFor="password" className="text-green-800 opacity-75">
+              Password
+            </label>
+            <input
+              {...register('password')}
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              className="rounded-lg p-4 outline-none shadow-md"
+            />
+            {errors.password && (
+              <p className="text-red-400">{errors.password.message}</p>
+            )}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-green-800 p-3 w-full rounded-3xl text-slate-50 shadow-lg"
+        >
+          Log In
+        </button>
+      </form>
+      <div className="flex items-center justify-center flex-col text-sm text-green-800 opacity-75">
+        <p>
+          You do not have an account?
+          <Link
+            href="sign-up"
+            className="hover:text-lime-700 transition font-semibold"
+          >
+            {' '}
+            Sign up here.
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
