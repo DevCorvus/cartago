6ee939b8d2c1a3ac814e360d9652da35f23d994b
DevCorvus <devcorvus.contact@gmail.com>
2024-04-05 13:53:34 -0400

    feat: add default error message to toast error

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,8 +1,14 @@
 import { isErrorWithMessage } from '@/utils/error';
 import toast from 'react-hot-toast';
 
-export function toastError(err: unknown) {
-  if (isErrorWithMessage(err)) {
-    toast.error(err.message);
+export function toastError(err?: unknown) {
+  let message: string;
+
+  if (err && isErrorWithMessage(err)) {
+    message = err.message;
+  } else {
+    message = 'Something went wrong';
   }
+
+  toast.error(message + ' .(');
 }
