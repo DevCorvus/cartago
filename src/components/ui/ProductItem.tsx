3c7b0e78cfdb454aca4d565c35d4850c22dd9a0a
DevCorvus <devcorvus.contact@gmail.com>
2024-05-02 13:36:49 -0400

    feat(product-item): add "new" tag, hover effect and other styling improvements

--- src/components/ui/ProductItem.tsx
+++ src/components/ui/ProductItem.tsx
@@ -3,25 +3,39 @@ import { ProductCardDto } from '@/shared/dtos/product.dto';
 import Image from 'next/image';
 import Link from 'next/link';
 import Rating from './Rating';
+import { useMemo } from 'react';
+
+const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 interface Props {
   product: ProductCardDto;
 }
 
 export default function ProductItem({ product }: Props) {
+  const isNew = useMemo(() => {
+    const createdAt = new Date(product.createdAt);
+    const createdAtInMs = createdAt.getTime();
+    return Date.now() < createdAtInMs + ONE_WEEK;
+  }, [product.createdAt]);
+
   return (
     <Link
       href={'/items/' + product.id}
       key={product.id}
-      className="flex flex-col rounded-lg bg-white shadow-md"
+      className="group relative flex flex-col rounded-lg border-b-2 border-r-2 border-gray-100 bg-white shadow-md"
     >
-      <div className="relative h-40 w-full rounded-t-lg bg-neutral-100">
+      {isNew && (
+        <span className="absolute -left-3 -top-3 z-10 rounded-md bg-lime-100 px-2 py-1 text-xs text-lime-600 shadow-md">
+          New
+        </span>
+      )}
+      <div className="relative h-40 w-full overflow-hidden rounded-t-lg bg-neutral-100">
         <Image
           src={'/images/' + product.images[0].path}
           alt={product.title}
           fill={true}
           sizes="200px"
-          className="object-contain"
+          className="object-contain transition duration-300 group-hover:scale-110"
         />
       </div>
       <section className="relative flex flex-1 flex-col gap-1 p-2">
@@ -29,7 +43,7 @@ export default function ProductItem({ product }: Props) {
           <h2 className="line-clamp-1 font-medium" title={product.title}>
             {product.title}
           </h2>
-          <span className="rounded-xl bg-lime-50 px-1 text-lg font-semibold text-lime-600 shadow-sm">
+          <span className="rounded-xl bg-lime-100 px-1 text-lg font-semibold text-lime-600 shadow-sm">
             {formatMoney(product.price)}
           </span>
         </header>
