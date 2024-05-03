832cf22d567d8755987a21ed7264a9d3fb33db92
DevCorvus <devcorvus.contact@gmail.com>
2024-05-02 21:58:10 -0400

    feat(navbar): add main icon

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -5,6 +5,7 @@ import SearchForm from './SearchForm';
 import { HiShoppingCart, HiUser } from 'react-icons/hi2';
 import { useState } from 'react';
 import Menu from './Menu';
+import CartagoIcon from './svg/CartagoIcon';
 
 export default function NavBar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -12,9 +13,14 @@ export default function NavBar() {
   return (
     <div>
       <div className="fixed z-50 w-full bg-lime-500 px-4 py-2 font-semibold text-white shadow-md">
-        <nav className="container mx-auto flex items-center justify-between relative">
+        <nav className="container relative mx-auto flex items-center justify-between">
           <header className="flex justify-center">
-            <Link href="/">Cartago</Link>
+            <Link href="/" className="flex items-center gap-2">
+              <div className="size-8 flex items-center justify-center rounded-full border-2 border-white">
+                <CartagoIcon className="w-3.5 text-white" />
+              </div>
+              <span className="text-xl">Cartago</span>
+            </Link>
           </header>
           <SearchForm />
           <ul className="flex items-center justify-center gap-5 text-2xl">
