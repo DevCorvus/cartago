2b4409489b2a772d5f0a419d95fd79158043328b
DevCorvus <devcorvus.contact@gmail.com>
2024-05-16 17:16:10 -0400

    feat(rating): handle fractional rating and some refactor

--- src/components/ui/Rating.tsx
+++ src/components/ui/Rating.tsx
@@ -1,29 +1,58 @@
+import { useMemo } from 'react';
 import { HiMiniStar, HiOutlineStar } from 'react-icons/hi2';
+
+enum Star {
+  Empty,
+  Half,
+  Full,
+}
+
+function getStarIcon(star: Star) {
+  switch (star) {
+    case Star.Empty: {
+      return <HiOutlineStar />;
+    }
+    case Star.Half: {
+      return (
+        <span className="flex">
+          <span className="overflow-hidden">
+            <HiMiniStar className="-mr-2" />
+          </span>
+          <span className="overflow-hidden">
+            <HiOutlineStar className="-ml-2" />
+          </span>
+        </span>
+      );
+    }
+    case Star.Full: {
+      return <HiMiniStar />;
+    }
+  }
+}
 
 interface Props {
   score: number;
 }
 
 export default function Rating({ score }: Props) {
+  const stars = useMemo(() => {
+    const fullStars = Math.floor(score);
+    const hasHalf = score % 1 >= 0.5;
+    const emptyStars = 5 - fullStars - (hasHalf ? 1 : 0);
+
+    return Array(fullStars)
+      .fill(Star.Full)
+      .concat(hasHalf ? [Star.Half] : [], Array(emptyStars).fill(Star.Empty));
+  }, []);
+
   return (
     <ul
       title={`Rating ${score}/5`}
       className="flex items-center text-yellow-500"
     >
-      {Array(score)
-        .fill(null)
-        .map((_, i) => (
-          <li key={Date.now() + i}>
-            <HiMiniStar />
-          </li>
-        ))}
-      {Array(5 - score)
-        .fill(null)
-        .map((_, i) => (
-          <li key={Date.now() * 2 + i}>
-            <HiOutlineStar />
-          </li>
-        ))}
+      {stars.map((star, i) => (
+        <li key={Date.now() + i}>{getStarIcon(star)}</li>
+      ))}
     </ul>
   );
 }
