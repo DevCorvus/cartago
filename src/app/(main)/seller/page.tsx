5af70122d9d6d5f6f700f18c72529afd82467fae
DevCorvus <devcorvus.contact@gmail.com>
2024-05-24 19:56:58 -0400

    feat: make seller page redirect to seller/products page

--- src/app/(main)/seller/page.tsx
+++ src/app/(main)/seller/page.tsx
@@ -1,25 +1,5 @@
-import { Permissions } from '@/shared/auth/rbac';
-import withAuth from '@/server/middlewares/withAuth';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-function Seller() {
-  return (
-    <div className="flex w-full max-w-md flex-col gap-6">
-      <header className="w-full">
-        <h1 className="text-2xl font-bold text-green-800">Seller</h1>
-      </header>
-      <ul>
-        <li>
-          <Link
-            href="/seller/products"
-            className="text-blue-400 transition hover:text-blue-500 focus:text-blue-500"
-          >
-            Products
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+export default function Seller() {
+  redirect('/seller/products');
 }
-
-export default withAuth(Seller, [Permissions.VIEW_SELLER_PANEL]);
