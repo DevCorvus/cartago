7f263899f06ca0daffdc5166e435468d8e89ff29
DevCorvus <devcorvus.contact@gmail.com>
2024-05-25 17:43:40 -0400

    feat: make admin page redirect to admin/categories page

--- src/app/(main)/admin/page.tsx
+++ src/app/(main)/admin/page.tsx
@@ -1,21 +1,5 @@
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Admin() {
-  return (
-    <div className="flex w-full max-w-md flex-col gap-6">
-      <header className="w-full">
-        <h1 className="text-2xl font-bold text-green-800">Admin</h1>
-      </header>
-      <ul>
-        <li>
-          <Link
-            href="/admin/categories"
-            className="text-blue-400 transition hover:text-blue-500 focus:text-blue-500"
-          >
-            Categories
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+  redirect('/admin/categories');
 }
