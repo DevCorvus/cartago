8f2135c123d5a34a17c571443766c35e098007fe
DevCorvus <devcorvus.contact@gmail.com>
2024-05-27 17:21:38 -0400

    feat(seller-product-list): add link to create new product and handle empty state

--- src/components/ui/SellerProductList.tsx
+++ src/components/ui/SellerProductList.tsx
@@ -8,6 +8,8 @@ import { useDeleteProduct, useProductCards } from '@/data/product';
 import SomethingWentWrong from './SomethingWentWrong';
 import { toastError } from '@/lib/toast';
 import SellerProductItem from './SellerProductItem';
+import Link from 'next/link';
+import { HiMiniPlus } from 'react-icons/hi2';
 
 export default function SellerProductList() {
   const [products, setProducts] = useState<ProductCardDto[]>([]);
@@ -51,20 +53,31 @@ export default function SellerProductList() {
 
   return (
     <div className="flex w-full flex-col gap-6">
-      <header>
+      <header className="flex justify-between">
         <h1 className="text-2xl font-bold text-cyan-700">Your products</h1>
+        <Link
+          href="/items/new"
+          className="btn inline-flex items-center gap-1 px-3 py-2"
+        >
+          <HiMiniPlus className="text-2xl" />
+          New
+        </Link>
       </header>
       <SearchInput term="products" handleSearch={searchProducts} alternative />
-      <ul className="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:grid-cols-4 xl:grid-cols-5">
-        {selectedProducts.map((product) => (
-          <li key={product.id}>
-            <SellerProductItem
-              product={product}
-              deleteProduct={() => deleteProduct(product.id)}
-            />
-          </li>
-        ))}
-      </ul>
+      {products.length > 0 ? (
+        <ul className="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:grid-cols-4 xl:grid-cols-5">
+          {selectedProducts.map((product) => (
+            <li key={product.id}>
+              <SellerProductItem
+                product={product}
+                deleteProduct={() => deleteProduct(product.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-slate-500">You do not have any products yet.</p>
+      )}
     </div>
   );
 }
